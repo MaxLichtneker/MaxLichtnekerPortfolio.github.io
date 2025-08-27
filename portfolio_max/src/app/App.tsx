a34858755231484d5aed4ba655f7bd2885d8d5e0
MaxLichtneker <maxwil2001@hotmail.com>
2025-08-27 12:06:54 +0200

    added a couple more gifs for certain projects

--- portfolio_max/src/app/App.tsx
+++ portfolio_max/src/app/App.tsx
@@ -10,9 +10,9 @@ function App() {
       <video className='video-style' src={require("../assets/blockIt.mp4")} autoPlay muted loop></video>
       <h1>Projects</h1>
       <div className='Description-style'>
-        <ProjectDescription imageName='../Assets/malaga_Jam/FixMyTattoo_Gif_1.gif' projectName='a.s.r VR BURNOUT EXPERIENCE' projectDescription='I made a game for a.s.r I was lead developer, teaching 3 others how to use Unity while building improtant game systems like the day and night cycle and game manager'/>
+        <ProjectDescription imageName='../Assets/a.s.rProject/a.s.r_Burnout_VR.gif' projectName='a.s.r VR BURNOUT EXPERIENCE' projectDescription='I made a game for a.s.r I was lead developer, teaching 3 others how to use Unity while building improtant game systems like the day and night cycle and game manager'/>
         <ProjectDescription imageName='../Assets/malaga_Jam/FixMyTattoo_Gif_1.gif' projectName='OVERGROWN' projectDescription='Overgrown was a game I made at school with a group of 4 I really hope this appears below the image now'/>
-        <ProjectDescription imageName='../Assets/malaga_Jam/FixMyTattoo_Gif_1.gif' projectName='SWITCH' projectDescription='A multiplayer pvp game I made during my time at Grey Games'/>
+        <ProjectDescription imageName='../Assets/switch_Project/Switchgameplay.gif' projectName='SWITCH' projectDescription='A multiplayer pvp game I made during my time at Grey Games'/>
       </div>
     </div>
   );
