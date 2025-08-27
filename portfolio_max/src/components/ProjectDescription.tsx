036a4ef6df6c64dbb732da8758044f2860d25886
MaxLichtneker <maxwil2001@hotmail.com>
2025-08-27 11:42:54 +0200

    added ability to edit individual component title, image and description

--- portfolio_max/src/components/ProjectDescription.tsx
+++ portfolio_max/src/components/ProjectDescription.tsx
@@ -1,11 +1,18 @@
 import '../style/ProjectDescription.css';
 import testImage from '../assets/malaga_Jam/MalagaJam_Gif_2.gif'
 
-function ProjectDescription(){
-    return <div className='ProjectDescription-style'>
-    <img className='description-gif-syle' src={testImage}/>
-    <p>hello this is test text to see where it will appear</p>
-    </div>
+interface DescriptionProperties{
+    imageName: string;
+    projectName: string;
+    projectDescription: string;
 }
 
+function ProjectDescription(props:DescriptionProperties){
+    return <div className='ProjectDescription-style'>
+    <img className='description-gif-syle' src={props.imageName}/>
+    <h1>{props.projectName}</h1>
+    <p className='p1'>{props.projectDescription}</p>
+    </div>
+}
+//Overgrown was a game I made at school with a group of 4 I really hope this appears below the image now
 export default ProjectDescription;
