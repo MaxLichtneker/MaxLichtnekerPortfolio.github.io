3927ed8011f5a9f72b83975a6aeb441f4ad52e29
MaxLichtneker <maxwil2001@hotmail.com>
2025-08-27 10:42:48 +0200

    added images to certain project folders

--- portfolio_max/src/components/ProjectDescription.tsx
+++ portfolio_max/src/components/ProjectDescription.tsx
@@ -1,7 +1,11 @@
 import '../style/ProjectDescription.css';
+import testImage from '../assets/malaga_Jam/MalagaJam_Gif_2.gif'
 
 function ProjectDescription(){
-    return <div className='ProjectDescription-style'></div>
+    return <div className='ProjectDescription-style'>
+    <img className='description-gif-syle' src={testImage}/>
+    <p>hello this is test text to see where it will appear</p>
+    </div>
 }
 
 export default ProjectDescription;
