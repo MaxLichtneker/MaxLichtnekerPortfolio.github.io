df9760e86022a733690e319ea1fd47e7a03e5078
MaxLichtneker <maxwil2001@hotmail.com>
2025-09-03 14:18:29 +0200

    Updates

--- portfolio_max/src/components/Message.tsx
+++
@@ -1,5 +0,0 @@
-function Message(){
-return <h1>Max Lichtneker</h1>
-}
-
-export default Message;
--- portfolio_max/src/components/NavBar.tsx
+++
@@ -1,8 +0,0 @@
-import '../style/Navbar.css'
-import Message from './Message';
-function NavBar(){
-    return <div className="Navbar">
-    <Message/>
-    </div>
-}
-export default NavBar;
