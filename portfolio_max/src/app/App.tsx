324d8e4113a2f25f5fd118debfe01f1a09a23e68
MaxLichtneker <maxwil2001@hotmail.com>
2025-09-03 14:32:50 +0200

    looking for a fix to images

--- portfolio_max/src/app/App.tsx
+++ portfolio_max/src/app/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <video className='video-style' src={require("../assets/blockIt.mp4")} autoPlay muted loop></video>
       <h1>Projects</h1>
       <div className='Description-style'>
-        <ProjectDescription imageName={require('../assets/a.s.rProject/a.s.r_Burnout_VR.gif')} projectName='a.s.r VR BURNOUT EXPERIENCE' projectDescription='I made a game for a.s.r I was lead developer, teaching 3 others how to use Unity while building improtant game systems like the day and night cycle and game manager'/>
+        <ProjectDescription imageName={require('../a.s.rProject/a.s.r_Burnout_VR.gif')} projectName='a.s.r VR BURNOUT EXPERIENCE' projectDescription='I made a game for a.s.r I was lead developer, teaching 3 others how to use Unity while building improtant game systems like the day and night cycle and game manager'/>
         <ProjectDescription imageName='../public/Assets/malaga_Jam/FixMyTattoo_Gif_1.gif' projectName='OVERGROWN' projectDescription='Overgrown was a game I made at school with a group of 4 I really hope this appears below the image now'/>
         <ProjectDescription imageName='../public/Assets/switch_Project/Switchgameplay.gif' projectName='SWITCH' projectDescription='A multiplayer pvp game I made during my time at Grey Games'/>
       </div>
