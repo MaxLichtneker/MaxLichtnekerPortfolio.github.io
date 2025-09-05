7f6e033459bccf49ecef1e1624a8783478ee1504
MaxLichtneker <maxwil2001@hotmail.com>
2025-09-05 14:48:46 +0200

    small update

--- portfolio_max/src/app/App.tsx
+++ portfolio_max/src/app/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <ProjectDescription imageName={require('../assets/a.s.rProject/a.s.r_Burnout_VR.gif')} projectName='A.S.R VR BURNOUT EXPERIENCE' projectDescription='I made a game for a.s.r I was lead developer, teaching 3 others how to use Unity while building improtant game systems like the day and night cycle and game manager' projectYear='2025' projectSoftware='Unity' projectLength='6 months'/>
         <ProjectDescription imageName={require('../assets/volcano_Fest/Volcano_Fest_Clip (1).gif')} projectName='VOLCANO FEST' projectDescription='' projectYear='2025' projectSoftware='Unity' projectLength='6 months'/>
         <ProjectDescription imageName={require('../assets/switch_Project/Switchgameplay.gif')} projectName='SWITCH' projectDescription='A multiplayer pvp game I made during my time at Grey Games' projectYear='2020-2021' projectSoftware='Unreal Engine' projectLength='1 Year'/>
-        <ProjectDescription imageName={require('../assets/itchio_Game/Overgrown_Clip.gif')} projectName='OVERGROWN' projectDescription='Overgrown was a game I made at school with a group of 4 I really hope this appears below the image now' projectYear='2020' projectSoftware='Unity' projectLength='6 months'/>
+        <ProjectDescription imageName={require('../assets/itchio_Game/Overgrown_Clip.gif')} projectName='OVERGROWN' projectDescription='Overgrown was a game I made at school with a group of 4 I really hope this appears below the image now' projectYear='2020' projectSoftware='Unity' projectLength='3 months'/>
       </div>
       <h1>Game Jams</h1>
       <div className='Description-style'>
