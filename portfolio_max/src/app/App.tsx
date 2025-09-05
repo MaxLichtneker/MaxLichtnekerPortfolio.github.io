0f924f941ccb62163684bc3572a7fa17284a31f6
MaxLichtneker <maxwil2001@hotmail.com>
2025-09-05 14:47:59 +0200

    added smal tab with basic information of dates and times of projects

--- portfolio_max/src/app/App.tsx
+++ portfolio_max/src/app/App.tsx
@@ -10,20 +10,20 @@ function App() {
       <video className='video-style' src={require("../assets/blockIt.mp4")} autoPlay muted loop></video>
       <h1>Projects</h1>
       <div className='Description-style'>
-        <ProjectDescription imageName={require('../assets/a.s.rProject/a.s.r_Burnout_VR.gif')} projectName='a.s.r VR BURNOUT EXPERIENCE' projectDescription='I made a game for a.s.r I was lead developer, teaching 3 others how to use Unity while building improtant game systems like the day and night cycle and game manager'/>
-        <ProjectDescription imageName={require('../assets/volcano_Fest/Volcano_Fest_Clip (1).gif')} projectName='Volcano Fest' projectDescription=''/>
-        <ProjectDescription imageName={require('../assets/switch_Project/Switchgameplay.gif')} projectName='SWITCH' projectDescription='A multiplayer pvp game I made during my time at Grey Games'/>
-        <ProjectDescription imageName={require('../assets/itchio_Game/Overgrown_Clip.gif')} projectName='OVERGROWN' projectDescription='Overgrown was a game I made at school with a group of 4 I really hope this appears below the image now'/>
+        <ProjectDescription imageName={require('../assets/a.s.rProject/a.s.r_Burnout_VR.gif')} projectName='A.S.R VR BURNOUT EXPERIENCE' projectDescription='I made a game for a.s.r I was lead developer, teaching 3 others how to use Unity while building improtant game systems like the day and night cycle and game manager' projectYear='2025' projectSoftware='Unity' projectLength='6 months'/>
+        <ProjectDescription imageName={require('../assets/volcano_Fest/Volcano_Fest_Clip (1).gif')} projectName='VOLCANO FEST' projectDescription='' projectYear='2025' projectSoftware='Unity' projectLength='6 months'/>
+        <ProjectDescription imageName={require('../assets/switch_Project/Switchgameplay.gif')} projectName='SWITCH' projectDescription='A multiplayer pvp game I made during my time at Grey Games' projectYear='2020-2021' projectSoftware='Unreal Engine' projectLength='1 Year'/>
+        <ProjectDescription imageName={require('../assets/itchio_Game/Overgrown_Clip.gif')} projectName='OVERGROWN' projectDescription='Overgrown was a game I made at school with a group of 4 I really hope this appears below the image now' projectYear='2020' projectSoftware='Unity' projectLength='6 months'/>
       </div>
       <h1>Game Jams</h1>
       <div className='Description-style'>
-        <ProjectDescription imageName={require('../assets/malaga_Jam/FixMyTattoo_Gif_1.gif')} projectName='Malaga Jam' projectDescription=''/>
-        <ProjectDescription imageName={require('../assets/egg_Game_Jam/Egg_Game_Jam_clip.gif')} projectName='School Game Jam theme EGG' projectDescription=''/>
+        <ProjectDescription imageName={require('../assets/malaga_Jam/FixMyTattoo_Gif_1.gif')} projectName='MALAGA JAM' projectDescription='' projectYear='2020' projectSoftware='Unity' projectLength='3 days'/>
+        <ProjectDescription imageName={require('../assets/egg_Game_Jam/Egg_Game_Jam_clip.gif')} projectName='SCHOOL GAME JAM THEME EGG' projectDescription='' projectYear='2025' projectSoftware='Unreal Engine' projectLength='1 week'/>
       </div>
       <h1>Web Development</h1>
         <div className='Description-style'>
-          <ProjectDescription imageName={require('../assets/portfolio_Clips/Gabriella_Portfolio_Clip.gif')} projectName='Portfolio Gabriella Lichtneker' projectDescription=''/>
-          <ProjectDescription imageName={require('../assets/portfolio_Clips/Anne_portfolio_Clip.gif')} projectName='Portfolio Anne Roos Leeuwis' projectDescription=''/>
+          <ProjectDescription imageName={require('../assets/portfolio_Clips/Gabriella_Portfolio_Clip.gif')} projectName='Portfolio Gabriella Lichtneker' projectDescription='' projectYear='2024' projectSoftware='HTML-CSS' projectLength='2 weeks'/>
+          <ProjectDescription imageName={require('../assets/portfolio_Clips/Anne_portfolio_Clip.gif')} projectName='Portfolio Anne Roos Leeuwis' projectDescription='' projectYear='2024' projectSoftware='HTML-CSS' projectLength='2 weeks'/>
         </div>
       <h1>About me</h1> 
     </div>
--- portfolio_max/src/components/ProjectDescription.tsx
+++ portfolio_max/src/components/ProjectDescription.tsx
@@ -5,13 +5,23 @@ interface DescriptionProperties{
     imageName: string;
     projectName: string;
     projectDescription: string;
+    projectYear:string;
+    projectSoftware:string;
+    projectLength:string;
 }
 
 function ProjectDescription(props:DescriptionProperties){
     return <div className='ProjectDescription-style'>
     <img className='description-gif-syle' src={props.imageName}/>
-    <h1>{props.projectName}</h1>
-    <p className='p1'>{props.projectDescription}</p>
+        <div className='description-information'>
+            <p>year: {props.projectYear}</p>
+            <p>software: {props.projectSoftware}</p>
+            <p>project length: {props.projectLength}</p>
+        </div>
+        <div className='description-text-box'>
+            <h1 className='description-header'>{props.projectName}</h1>
+            <p className='p1'>{props.projectDescription}</p>
+        </div>
     </div>
 }
 //Overgrown was a game I made at school with a group of 4 I really hope this appears below the image now
