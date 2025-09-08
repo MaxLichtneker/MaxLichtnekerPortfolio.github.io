1f450bd84665520d12d50a7902d1045edab57931
MaxLichtneker <maxwil2001@hotmail.com>
2025-09-08 12:31:03 +0200

    fix

--- portfolio_max/src/index.tsx
+++ portfolio_max/src/index.tsx
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <HashRouter>
-      <App/>
-  </HashRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
